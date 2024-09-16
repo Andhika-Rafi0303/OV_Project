940fd6b2a8a6ae02f32f0651d802873237a61fbb
“Rafi030615” <rafiandhika@student.telkomuniversity.ac.id>
2024-09-16 22:22:56 +0700

    update

--- app.py
+++ app.py
@@ -89,14 +89,14 @@ phrases = [
 # Centered input and button
 with st.container():
     # Create two columns for side-by-side layout
-    col1, col2 = st.columns([2, 3], vertical_alignment = 'bottom')  # Adjust column widths as needed
+    col1, col2 = st.columns([1, 3], vertical_alignment = 'bottom')  # Adjust column widths as needed
 
     # Input field in the first column
-    with col2:
+    with col1:
         token_input = st.text_input("Input Passenger ID")
 
     # Submit button in the second column
-    with col1:
+    with col2:
         if st.button("Submit"):
             if token_input:
                 url = tokens_dict.get(token_input)
